5020d7e8c1378cc82ab3165f6abf3540e98dd3c3
Matan Yechiel <matan@rele.ai>
2021-05-29 12:54:54 +0300

    firebase hosting

--- src/configs.js
+++ src/configs.js
@@ -1,16 +1,16 @@
 // define movies configs
 export const OMDB_CONFIGS = {
   apikey: "dce24c91",
-  baseURL: "http://www.omdbapi.com",
+  baseURL: "https://www.omdbapi.com",
   apiLimit: 10
 }
 
 // define weather configs
 export const OPEN_WEATHER_CONFIGS = {
   apikey: "c2152ce33eec94f628bcb40cda3da446",
-  baseURL: "http://api.openweathermap.org/data/2.5/weather",
+  baseURL: "https://api.openweathermap.org/data/2.5/weather",
   fallbackCity: "tel-aviv",
-  iconBaseURL: "http://openweathermap.org/img/wn",
+  iconBaseURL: "https://openweathermap.org/img/wn",
   defaultUnitTemp: "metric"
 }
 
